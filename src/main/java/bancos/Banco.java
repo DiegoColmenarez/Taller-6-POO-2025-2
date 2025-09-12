44c481c9b7db4dd7f6d8fda3b996fdf75e033f35
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2025-09-11 23:14:42 -0500

    Tercer ejercicio completado

--- src/main/java/bancos/Banco.java
+++ src/main/java/bancos/Banco.java
@@ -1,4 +1,27 @@
 package bancos;
 
 public class Banco {
+    protected double saldo;
+    public Banco(double saldo) {
+        this.saldo = saldo;
+    }
 }
+class Hack extends Banco {
+    public Hack(double saldo){
+        super(saldo);
+    }
+    public void robar(){
+        this.saldo = 999999;
+    }
+
+    public static void main(String[] args) {
+        var usuarioSinSaldo = new Banco(0);
+        System.out.println("Dinero del usuario: " + usuarioSinSaldo.saldo);
+        System.out.println("Usuario despues de hackear:");
+        var eseMismoUsuario = new Hack(0);
+        eseMismoUsuario.robar();
+        System.out.println("Dinero del usuario: " + eseMismoUsuario.saldo);
+    }
+}
+
+
